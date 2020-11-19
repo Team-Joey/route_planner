cefe2d22c9c30ee10ebe751d3bd50e67a574df97
reni-hristova <rxh794@student.bham.ac.uk>
2020-11-19 23:28:05 +0000

    Node instance created, which spins until the task (boolean) is False

--- route_planner/scripts/node.py
+++ route_planner/scripts/node.py
@@ -7,10 +7,17 @@ import 	route_planner.rp
 
 class RoutePlannerNode(object):
     def __init__(self):
-	rospy.loginfo("Success! init function in node.py.")
-
+	rospy.loginfo("Success! init function in node.py.") # Information is only logged after a node instance has been created
+	print("Initialisation successful.")
 
 
 if __name__ == '__main__':
+    rospy.init_node('dummy_node', anonymous = True) 	# (anonymous = True) ensures the name is unique for each node 
+    node = RoutePlannerNode() 				# Node initialisation
     rospy.loginfo("Success! Main function in node.py.")
-    print("Dummy was successful. Module was connected.")
+    
+    task_incomplete = True
+    while task_incomplete:				# Node is active until the task has been completed
+	rospy.spin()
+    
+    print("End.")
