9ef997633193ea5ce1c17b438a6620ab1faa7a1f
reni-hristova <rxh794@student.bham.ac.uk>
2020-11-20 00:51:57 +0000

    Basic initialisation functions for the node and the script added. Added the util.py file to perform rotations on Quaternions. Removed initial pose as it will be known.

--- route_planner/scripts/node.py
+++ route_planner/scripts/node.py
@@ -4,20 +4,38 @@ import 	rospy
 import 	route_planner
 import 	route_planner.rp
 
+import 	sys
+from 	geometry_msgs.msg 	import PoseStamped
+from 	tf.msg 			import tfMessage
+# from 	sensor_msgs.msg 	import LaserScan
+# from 	nav_msgs.msg 		import OccupancyGrid, Odometry
+
 
 class RoutePlannerNode(object):
     def __init__(self):
-	rospy.loginfo("Success! init function in node.py.") # Information is only logged after a node instance has been created
-	print("Initialisation successful.")
+	rospy.loginfo("Node initialisation...") 				# Information is only logged after a node instance has been created
 
+	# ----- Minimum change DELTA before publishing a pose
+	self._PUBLISH_DELTA = rospy.get_param("publish_delta", 0.1)
+	self._route_planner = route_planner.rp.RoutePlanner() 
+	self._route_planner.dummy_function_sum(3, 4)				# Accessing a function in the script
 
+	self._latest_scan = None
+        self._last_published_pose = None
+	
+	self._pose_publisher = rospy.Publisher("/current_pose", PoseStamped, queue_size=1)	# Publishes the position of the robot for visualisaion
+        self._tf_publisher = rospy.Publisher("/tf", tfMessage, queue_size=1)			# Publishes tf message for debugging
+
+	# ----- Then set the occupancy grid map
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     rospy.init_node('dummy_node', anonymous = True) 	# (anonymous = True) ensures the name is unique for each node 
+    rospy.loginfo("Creating the node instance...")
     node = RoutePlannerNode() 				# Node initialisation
-    rospy.loginfo("Success! Main function in node.py.")
     
     task_incomplete = True
     while task_incomplete:				# Node is active until the task has been completed
 	rospy.spin()
-    
+    rospy.loginfo("Task has been completed.")
     print("End.")
--- route_planner/src/route_planner/rp.py
+++ route_planner/src/route_planner/rp.py
@@ -1,7 +1,37 @@
+import 	rospy
+from 	geometry_msgs.msg 	import PoseStamped, Quaternion
+from 	nav_msgs.msg 		import OccupancyGrid
+from 	util 			import rotateQuaternion, getHeading
+from 	tf.msg 			import tfMessage
+
+
 class RoutePlanner(object):
+    START_X = 0
+    START_Y = 0
+    START_Z = 0
+    START_HEADING = 0
 
     def __init__(self):
-	rospy.loginfo("Success! You just have imported a Python module in another package.")
+	rospy.loginfo("A route planner object was created.")
+	self.current_pose = PoseStamped()			# Current pose of the robot
+	self.occupancy_map = OccupancyGrid()			# Occupancy Grid map
+	self.tf_message = tfMessage()				# tf message for debugging
 
-    def dummy_function(self):
-	rospy.loginfo("Success! Dummy function activated.")
+	# ---- Setting the current pose based on the given starting point 
+	self.current_pose.pose.position.x = self.START_X
+        self.current_pose.pose.position.y = self.START_Y
+        self.current_pose.pose.position.z = self.START_Z
+        self.current_pose.pose.orientation = rotateQuaternion(Quaternion(w=1.0),
+                                                                    self.START_HEADING)
+	self.current_pose.header.frame_id = "/map"
+
+#--------------------------------Dummy-------------------------------------------------------------------------------------------------------------------
+
+    def dummy_function_sum(self, a, b):
+	"""
+	Dummy function. Sums 2 numbers.
+	"""
+	rospy.loginfo("Dummy function started")
+
+	print("The sum of ", a, " and ", b," is ", a+b)
+
